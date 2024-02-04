7c8afa1a57278ed6a74fbce3cfc1f158a69baa39
Andres Sepulveda <bodyweightforce@gmail.com>
2024-02-04 03:37:31 -0300

    make apis method for products

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,10 @@
 package main
 
+import "github.com/Andressep/QuoteMaker/internal/setup"
+
 func main() {
+	e := setup.InitializeApp()
+	// Inicia el servidor
+	e.Logger.Fatal(e.Start(":8080"))
 
 }
