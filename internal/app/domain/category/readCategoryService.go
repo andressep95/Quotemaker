8994340d5a47590a73778184273a1544514b2d98
Andressep <andressep.95@gmail.com>
2024-04-19 23:56:50 -0400

    git commit -m "Implement readCategoryService.go and errors.go modifications"

--- internal/app/domain/category/readCategoryService.go
+++ internal/app/domain/category/readCategoryService.go
@@ -6,6 +6,16 @@ type ReadCategoryService struct {
 	readCategoryService ReadCategoryRepository
 }
 
+func (s *ReadCategoryService) GetCategory(ctx context.Context, id string) (*Category, error) {
+	return s.readCategoryService.GetCategoryByID(ctx, id)
+}
+func (s *ReadCategoryService) GetCategoryByName(ctx context.Context, name string) (Category, error) {
+	return s.readCategoryService.GetCategoryByName(ctx, name)
+}
+
+func (s *ReadCategoryService) ListCategories(ctx context.Context, limit int, offset int) ([]Category, error) {
+	return s.readCategoryService.ListCategorys(ctx, limit, offset)
+}
 func NewReadCategoryService(readCategoryRepository ReadCategoryRepository) *ReadCategoryService {
 	return &ReadCategoryService{
 		readCategoryService: readCategoryRepository,
--- internal/pkg/errs/errors.go
+++ internal/pkg/errs/errors.go
@@ -2,4 +2,10 @@ package errs
 
 import "errors"
 
-var ErrCategoryNotFound = errors.New("categoría no encontrada")
+var (
+	ErrCategoryNotFound = errors.New("category not found")
+	ErrQuoteNotFound    = errors.New("quote not found")
+	ErrUserNotFound     = errors.New("user not found")
+	ErrInvalidInput     = errors.New("invalid input")
+	ErrDatabaseError    = errors.New("database error")
+)
