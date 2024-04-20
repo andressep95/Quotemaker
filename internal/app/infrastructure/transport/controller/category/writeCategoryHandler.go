d2145712eb09637c817e23aa1caff9d63c80833b
Andressep <andressep.95@gmail.com>
2024-04-20 00:59:54 -0400

    Add CRUD operations for category in writeCategoryHandler.go

--- internal/app/infrastructure/transport/controller/category/writeCategoryHandler.go
+++ internal/app/infrastructure/transport/controller/category/writeCategoryHandler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	application "github.com/Andressep/QuoteMaker/internal/app/application/category"
+	dto "github.com/Andressep/QuoteMaker/internal/app/dto/category"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,8 +12,10 @@ type WriteCategoryHandler struct {
 	writeCategoryUseCase *application.WriteCategoryUseCase
 }
 
-func (rw WriteCategoryHandler) WriteCategoryRouter(r *gin.Engine) {
+func (rw *WriteCategoryHandler) WriteCategoryRouter(r *gin.Engine) {
 	r.POST("/category", rw.CreateCategoryHandler)
+	r.PUT("/category", rw.UpdateCategoryHandler)
+	r.DELETE("/category/:id", rw.DeleteCategoryHandler)
 }
 
 func NewWriteCategoryHandler(writeCategoryUseCase *application.WriteCategoryUseCase) *WriteCategoryHandler {
@@ -23,7 +26,7 @@ func NewWriteCategoryHandler(writeCategoryUseCase *application.WriteCategoryUseC
 
 // CreateCategoryHandler maneja las solicitudes POST para crear nuevas categorías.
 func (rw *WriteCategoryHandler) CreateCategoryHandler(c *gin.Context) {
-	var req application.CreateCategoryRequest
+	var req dto.CreateCategoryRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -36,4 +39,33 @@ func (rw *WriteCategoryHandler) CreateCategoryHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, resp)
+}
+
+// UpdateCategoryHandler maneja las solicitudes PUT para actualizar categorías existentes.
+func (rw *WriteCategoryHandler) UpdateCategoryHandler(c *gin.Context) {
+	var req dto.UpdateCategoryRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	resp, err := rw.writeCategoryUseCase.UpdateCategory(c.Request.Context(), &req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, resp)
+}
+
+// DeleteCategoryHandler maneja las solicitudes DELETE para eliminar categorías existentes.
+func (rw *WriteCategoryHandler) DeleteCategoryHandler(c *gin.Context) {
+	categoryID := c.Param("id")
+	req := &dto.DeleteCategoryRequest{
+		ID: categoryID,
+	}
+	_, err := rw.writeCategoryUseCase.DeleteCategory(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Category deleted successfully"})
 }
